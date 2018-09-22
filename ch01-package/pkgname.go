3fe983f588b6c056f0896152717f4379b5913d63
master-genius <1146040444@qq.com>
2018-09-22 13:53:36 +0800

    --

---
+++ ch01-package/pkgname.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "xy"
+)
+
+func main (){
+    fmt.Println(xy.Name)
+}
+
---
+++ ch02-vars/var.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main () {
+    var a int = 12
+    var x int64 = 123456789012345
+    var b float32 = 12.34
+    var c float64 = 34.5
+    var s string = "go fast"
+
+    fmt.Println(a,x,b,c,s)
+}
+
